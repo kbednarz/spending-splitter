510d5aa29ad673e9f7fa18e568a839f7624bf1fb
kamex737 <kamex737@gmail.com>
2018-04-19 19:34:49 +0200

    basic security configuration

---
+++ src/main/java/com/github/kbednarz/spendingsplitter/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.github.kbednarz.spendingsplitter.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+//    @Autowired
+//    private AccessDeniedHandler accessDeniedHandler;
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/h2/**", "/", "/index").permitAll()
+                .antMatchers("/admin/**").hasAnyRole("ADMIN")
+                .antMatchers("/user/**").hasAnyRole("USER")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+//                .and()
+//                .exceptionHandling().accessDeniedHandler(accessDeniedHandler);
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("user").password("user").roles("USER")
+                .and()
+                .withUser("admin").password("admin").roles("ADMIN");
+    }
+}

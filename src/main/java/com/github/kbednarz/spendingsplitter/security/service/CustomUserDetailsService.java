5581e6a285bf873c960a9b6c95316b1e4b15d2a7
kamex737 <kamex737@gmail.com>
2018-04-22 10:38:35 +0200

    added redirect root to home

--- src/main/java/com/github/kbednarz/spendingsplitter/security/service/CustomUserDetailsService.java
+++ src/main/java/com/github/kbednarz/spendingsplitter/security/service/CustomUserDetailsService.java
@@ -3,6 +3,7 @@ package com.github.kbednarz.spendingsplitter.security.service;
 import com.github.kbednarz.spendingsplitter.security.dao.UserDAO;
 import com.github.kbednarz.spendingsplitter.security.domain.User;
 import com.github.kbednarz.spendingsplitter.security.repository.UserRepository;
+import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -24,6 +25,7 @@ public class CustomUserDetailsService implements UserDetailsService {
             throw new UsernameNotFoundException(username);
         }
 
+        Hibernate.initialize(user.getRoles());
         return new UserDAO(user);
     }
 }

9484e04f4eabc90382075d8770c8ae31f4a90955
kamex737 <kamex737@gmail.com>
2018-05-27 11:03:42 +0200

    ddl set to update, added info while starting

--- src/main/java/com/github/kbednarz/spendingsplitter/SpendingSplitterApplication.java
+++ src/main/java/com/github/kbednarz/spendingsplitter/SpendingSplitterApplication.java
@@ -1,12 +1,16 @@
 package com.github.kbednarz.spendingsplitter;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpendingSplitterApplication {
+    private static final Log log = LogFactory.getLog(SpendingSplitterApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpendingSplitterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpendingSplitterApplication.class, args);
+        log.info("---------- APPLICATION HAS STARTED ----------");
+    }
 }
